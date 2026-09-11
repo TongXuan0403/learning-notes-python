97ee9c2b78de5a8fb17344c6d1a6bc98f31cc0f3
Miku_Tog <TongXuan0403@outlook.com>
2026-09-11 20:11:25 +0800

    丰富的数据类型题目编写

---
+++ 刷题/59道基础入门题/丰富的数据类型/合并两个字典.py
@@ -0,0 +1,10 @@
+"""
+写一个程序：合并两个字典。
+
+创建字典A，内容为{12: 'Kathmandu', 11: 'London', 3: 'Sydney'}。
+创建字典B，内容为{10: 'New York', 2: 'Delhi'}。
+使用update()合并这两个字典并打印。
+示例输出
+{12: 'Kathmandu', 11: 'London', 3: 'Sydney', 10: 'New York', 2: 'Delhi'}
+
+"""
---
+++ 刷题/59道基础入门题/丰富的数据类型/集合的合集.py
@@ -0,0 +1,11 @@
+"""
+编写一个程序：计算两个集合的合集。
+
+创建developers集合，元素为"Mark", "George", "Ben"。
+创建designers的集合，元素为"Andy", "Jared", "Ben"。
+用union()函数返回一个新的集合，包含所有集合的元素。
+打印包含developers 和 designers 的合集。
+示例输出
+{'Ben', 'Mark', 'Andy', 'Jared', 'George'}
+
+"""
