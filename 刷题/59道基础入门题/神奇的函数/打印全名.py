d60b603ba504c4cbedec72c59ad596c26a1316c0
Miku_Tog <TongXuan0403@outlook.com>
2026-09-11 20:04:55 +0800

    神奇的函数题目编写

---
+++ 刷题/59道基础入门题/神奇的函数/打印全名.py
@@ -0,0 +1,18 @@
+"""
+编写一个函数：打印全名，中间有空格。
+
+定义一个名为full_name()的函数，有两个参数first_name和last_name。
+在函数中，打印 first_name 和 last_name ，并用一个空格将它们分开。
+在函数外部，输入名字和姓氏的字符串，并将赋值给first和last变量。
+调用该函数并将first和last作为参数传递。
+输入格式
+
+两个字符串
+
+示例输入
+Vance
+Joy
+示例输出
+Vance Joy
+
+"""
